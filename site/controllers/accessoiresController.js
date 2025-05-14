1c24f2f8e2a73032bf40740b011f780176bf2b57
saira <nkoukabanzouzisaira@gmail.com>
2025-05-15 00:57:42 +0200

    changement

--- site/controllers/accessoiresController.js
+++ site/controllers/accessoiresController.js
@@ -1,9 +1,25 @@
+const accessoires = require('../../data/products.json')
+
 const accessoiresController = {
     getaccessoiresPage : (req , res) =>{
-        res.render('accessoires.ejs')
+        res.render('accessoires.ejs' , {accessoires})
     },
 
+
+
+getaccessoiresDetails : (req , res) =>{ 
+    //recuperer l'ID de la catégorie concernée
+    const accessoireID = req.params.id;
+
+    console.log("id de accessoire : " + accessoireID)
+
+//const accessoirSearch = accessoires.find(accessoirSearch => accessoirSearch.id == accessoireID)
+        //res.render('accessoiresdetails' , {accessoirSearch});
 }
 
+
+
+
+}
 
 module.exports = accessoiresController;
